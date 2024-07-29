9a631302d380812d6eae2679e229ae22b52ed69c
MarvinKristMarvin <marvin.krist@oclock.school>
2024-07-29 20:02:35 +0200

    added search by name and by type

--- front/assets/js/components/searchMenu.js
+++ front/assets/js/components/searchMenu.js
@@ -4,6 +4,8 @@ const searchMenu = {
     const newTypeDiv = document.importNode(typeDivTemplate.content, true);
 
     newTypeDiv.querySelector("label").textContent = objectData.name;
+    newTypeDiv.querySelector("label").setAttribute("for", objectData.name);
+    newTypeDiv.querySelector("input").id = objectData.name;
     newTypeDiv.querySelector("label").style.color = "#" + objectData.color;
 
     document.querySelector(".type-selection").append(newTypeDiv);
@@ -26,6 +28,50 @@ const searchMenu = {
         results -= 1;
       }
     });
+
+    const resultPara = document.querySelector(".search-infos-div > p");
+    resultPara.textContent = results + " Pokemons found";
+  },
+
+  searchType(event) {
+    const typeCheckboxesElements =
+      document.querySelectorAll(".type-div > input");
+    typeCheckboxesElements.forEach((element) => {
+      element.checked = false;
+    });
+    event.target.checked = true;
+    const pokemonDivs = document.querySelectorAll(".pokemon-div");
+    let results = 0;
+    console.log(event.target.id);
+    if (event.target.id !== "All") {
+      pokemonDivs.forEach((pokemonDiv) => {
+        pokemonDiv.classList.remove("hidden");
+        const typeSpanElements = pokemonDiv.querySelectorAll(".types > span");
+        let alreadyFound = false;
+        typeSpanElements.forEach((element) => {
+          if (
+            element.textContent !== event.target.id &&
+            alreadyFound === false
+          ) {
+            pokemonDiv.classList.add("hidden");
+            // le pokemon n'appartient au type choisi
+          } else if (
+            element.textContent === event.target.id &&
+            alreadyFound === false
+          ) {
+            // il appartient
+            pokemonDiv.classList.remove("hidden");
+            results += 1;
+            alreadyFound = true;
+          }
+        });
+      });
+    } else {
+      pokemonDivs.forEach((pokemonDiv) => {
+        pokemonDiv.classList.remove("hidden");
+        results += 1;
+      });
+    }
 
     const resultPara = document.querySelector(".search-infos-div > p");
     resultPara.textContent = results + " Pokemons found";
--- front/assets/js/utils/listeners.js
+++ front/assets/js/utils/listeners.js
@@ -5,6 +5,11 @@ const listeners = {
   addListeners() {
     const searchBarElement = document.querySelector(".input-name");
     searchBarElement.addEventListener("keyup", searchMenu.searchName);
+
+    const typeCheckboxesElements = document.querySelectorAll(".type-div");
+    typeCheckboxesElements.forEach((element) => {
+      element.addEventListener("change", searchMenu.searchType);
+    });
   },
 };
 
